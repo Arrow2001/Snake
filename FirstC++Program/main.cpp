8a9a6cad872da4e3e65b97c7f791044cd51de3db
Arrowverse2001 <43183955+Arrowverse2001@users.noreply.github.com>
2019-01-16 19:57:07 +0000

    Snake
    This is my first program that I have coded in C++

--- FirstC++Program/main.cpp
+++ FirstC++Program/main.cpp
@@ -1,31 +1,105 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
+//Declaring Variables
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+
+void Setup()
+{
+	gameOver = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+
+}
+
+void Draw()
+{
+	system("cls"); //clears the command prompt window of previous messgaes. //system("claer");
+	for (int i = 0; i < width+2; i++)
+		cout << "#";
+	cout << endl;
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (j == 0)
+				cout << "#";
+			if (i == y && j == x)
+				cout << "O";
+			else if (i == fruitY && j == fruitX)
+				cout << "F";
+			else
+				cout << " ";
+
+
+
+			if (j == width - 1)
+				cout << "#";
+		}
+		cout << endl;
+	}
+
+		for (int i = 0; i < width+2; i++)
+			cout << "#";
+	cout << endl;
+}
+//Controls: W,A,S,D
+void Input()
+{
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+		case 'a':
+			dir = LEFT;
+			break;
+
+		case 'w':
+			dir = UP;
+			break;
+
+		case 's':
+			dir = DOWN;
+			break;
+
+		case 'd':
+			dir = RIGHT;
+			break;
+
+		case 'x':
+			gameOver = true;
+			break;
+		}
+	}
+}
+
+void Logic()
+{
+
+}
 
 int main()
 {
-	// First used when I started. Basic.
-#pragma region Basic
-	//cout << "Hello world";
-	//cout << " Some other text";
-	//system("pause");
-	//return 0;
-#pragma endregion
+	Setup();
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logic();
+		//Sleep(10); sleep(10);
 
-	
-	
-	// Work with variables etc.
-	double a;
-	double b, c;
-	a = 0;
-	b = 0;
-	cout << " Enter a: ";
-	cin >> a;
-	cout << " Enter b: ";
-	cin >> b;
-	c = a + b;
-	cout << " Value of varibale c is " << c << endl;
-	system("pause");
+	}
+
+
 	return 0;
-
-
 }
