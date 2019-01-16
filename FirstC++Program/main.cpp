394d9d92635d855bebcd66bde9cba4d480826ae5
Arrowverse2001 <43183955+Arrowverse2001@users.noreply.github.com>
2019-01-16 20:50:46 +0000

    Snake

--- FirstC++Program/main.cpp
+++ FirstC++Program/main.cpp
@@ -6,6 +6,8 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
 enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDirection dir;
 
@@ -24,7 +26,7 @@ void Setup()
 void Draw()
 {
 	system("cls"); //clears the command prompt window of previous messgaes. //system("claer");
-	for (int i = 0; i < width+2; i++)
+	for (int i = 0; i < width + 2; i++)
 		cout << "#";
 	cout << endl;
 
@@ -39,7 +41,21 @@ void Draw()
 			else if (i == fruitY && j == fruitX)
 				cout << "F";
 			else
-				cout << " ";
+			{
+				bool print = false;
+				for (int k = 0; k < nTail; k++)
+				{
+					
+					if (tailX[k] == j && tailY[k] == i)
+					{
+						cout << "o";
+						print = true;
+					}
+
+					if (!print)
+						cout << " ";
+				}
+			}
 
 
 
@@ -49,9 +65,15 @@ void Draw()
 		cout << endl;
 	}
 
-		for (int i = 0; i < width+2; i++)
-			cout << "#";
+	for (int i = 0; i < width + 2; i++)
+		cout << "#";
 	cout << endl;
+
+
+	for (int i = 0; i < width + 2; i++)
+		cout << "#";
+	cout << endl;
+	cout << "Score: " << score << endl;
 }
 //Controls: W,A,S,D
 void Input()
@@ -85,6 +107,45 @@ void Input()
 
 void Logic()
 {
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	for (int i = 1; i < nTail; i++)
+	{
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+	}
+	switch (dir)
+	{
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case UP:
+		y--;
+		break;
+	case DOWN:
+		y++;
+		break;
+	default:
+		break;
+	}
+	if (x > width || x < 0 || y > height || y < 0)
+		gameOver = true;
+	if (x == fruitX && y == fruitY)
+	{
+
+		score += 10;
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+		nTail++;
+	}
 
 }
 
